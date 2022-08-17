97aa1c395b29b9e3079b23b68f7bf7ef59809e33
wael1998 <wael.chakchouk@esprit.tn>
2022-08-17 01:55:12 +0100

    first commit

---
+++ js/script.js
@@ -0,0 +1,8 @@
+///////////////typing animation//////////////////////////
+
+var typed = new Typed(".typing",{
+    strings:["","IoT engineer","web devoloper","DevOps engineer "],
+    typeSpeed:100,
+    backSpeed:60,
+    loop:true
+})
